58dc7c8960473b13852e6f81b04c898ae17c8085
Bautista Rodriguez <bautirodri@gmail.com>
2023-09-13 21:17:21 +0200

    substr w/ no leaks :)

--- ft_substr.c
+++ ft_substr.c
@@ -6,25 +6,40 @@
 /*   By: bautrodr <bautrodr@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 20:25:59 by bautrodr          #+#    #+#             */
-/*   Updated: 2023/09/10 20:40:16 by bautrodr         ###   ########.fr       */
+/*   Updated: 2023/09/13 21:16:21 by bautrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static char	*new(size_t n)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (!str)
+		return (NULL);
+	return (str);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*subs;
-	size_t	i;
-	size_t	j;
+	char	*psubs;
 
-	i = start;
-	j = 0;
-	subs = (char *)malloc(len + 1);
-	if (!subs || !s)
+	if (!s)
 		return (NULL);
-	while (i < ft_strlen(s) && j < len)
-		subs[j++] = s[i++];
-	subs[j] = '\0';
-	return (subs);
+	if (start > ft_strlen(s))
+		len = 0;
+	else if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
+	subs = new(len);
+	if (!subs)
+		return (NULL);
+	s += start;
+	psubs = subs;
+	*(subs + len) = '\0';
+	while (len-- && *s)
+		*subs++ = *s++;
+	return (psubs);
 }
